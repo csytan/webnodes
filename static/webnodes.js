ce54cdde41e68d1fc8071826d0a636cf477adb75
Chris Tan <csytan@gmail.com>
2009-04-04 01:48:51 -0700

    Tree layout is now a jquery plugin. Nodes are now laid out in their parent container instead of the document body. Various bug fixes.

--- static/webnodes.js
+++ static/webnodes.js
@@ -1,21 +1,24 @@
-var WebNodes = function(root, graph, options){
-    // Fast Tree Layout    
+(function($){
+
+$.fn.treeLayout = function(graph, options){
+    var root = this.css({
+        left: 0,
+        width: this.parent().width()
+    });
+    var container = this.parent();
+    
     options = $.extend({
         minWidth: 400,
         linkColor: '#AFAFAF',
         linkWidth: 5,
         vertSpace: 20
     }, options);
-        
-    layout(root);
     
-    function layout(root) {
-        $(root).css({
-            left: 0,
-            width: '95%',
-            display: 'block'
-        });
-        var row = [initNode(root)];
+    layout();
+    
+    function layout() {
+        root.show();
+        var row = [initNode(root[0])];
         while (row) {
             row = removeNodes(row);
             row = removeNodes(row); // for those hard to reach areas
@@ -40,12 +43,12 @@ var WebNodes = function(root, graph, options){
         node.nKids = Math.min(node.maxKids, node.kids.length - node.start);
         node.needsSpace = node.kids.length - node.start > node.maxKids;
     }
-    
+
     function expand(node, node2) {
         if (node.needsSpace && 
             ((node.kidsTop + 50 > node2.kidsTop && !node2.kids.length) ||
             node.priority > node2.priority)) {
-            
+
             node.kidsLeft = Math.min(node.kidsLeft, node2.kidsLeft);
             node.kidsLeft = Math.min(node.kidsLeft, node2.kidsLeft);
             node.kidsWidth += node2.kidsWidth;
@@ -55,7 +58,7 @@ var WebNodes = function(root, graph, options){
         }
         return false;
     }
-    
+
     function removeNodes(row) {
         var next_row = [];
         for (var i=0, node; node=row[i]; i+=2){
@@ -72,7 +75,7 @@ var WebNodes = function(root, graph, options){
         }
         return next_row;
     }
-    
+
     function addNodes(row) {
         var lowest = row[0], index = 0;
         for (var i = 0, node; node = row[i]; i++) {
@@ -82,33 +85,33 @@ var WebNodes = function(root, graph, options){
             }
         }
         if (!lowest) return null;
-        
+
         var kids = layoutKids(lowest);
         var left = row.slice(0, index);
         var right = row.slice(index + 1);
         return left.concat(kids, right);
     }
-    
+
     function layoutKids(node) {
         var width = node.kidsWidth / node.nKids;
         node.kidsTop += options.vertSpace * node.nKids;
         var kids = [];
-        
+
         for (var i = 0, kid; i < node.nKids; i++) {
             var kid = document.getElementById(node.kids[node.start + i]);
             kid.style.top = node.kidsTop + 'px';
             kid.style.left = node.kidsLeft + (i * width) + 'px';
             kid.style.display = 'block';
             kid.style.width = width + 'px';
-            
+
             initNode(kid);
             kids.push(kid);
         }
-        
+
         drawConnections(node, kids);
         return kids;
     }
-    
+
     function showNavButtons(nodes) {
         for (var i=0, node; node=nodes[i]; i++) {
             $(node).find('.pagination').css('visibility', 'hidden');
@@ -121,7 +124,7 @@ var WebNodes = function(root, graph, options){
             } else {
                 $(node).find('a.next, a.expand').css('visibility', 'hidden');
             }
-        
+
             if (node.start) {
                 $(node).find('.pagination').css('visibility', 'visible');
                 $(node).find('a.prev').css('visibility', 'visible')
@@ -131,33 +134,33 @@ var WebNodes = function(root, graph, options){
             }
         }
     }
-    
+
     function drawConnections(node, kids) {
         var x = node.offsetLeft + node.offsetWidth / 2 - node.kidsLeft;
         var y = node.offsetTop + node.offsetHeight - 5;
         var height = node.kidsTop - node.offsetTop - node.offsetHeight + 10;
         var canvas = document.createElement('canvas');
-        
+
         canvas.width = node.kidsWidth;
         canvas.height = height;
 
         $(canvas).css({
             top: y,
             left: node.kidsLeft
-        }).appendTo(document.body);
-        
+        }).appendTo(container);
+
         // Initialize Excanvas
         if (window.G_vmlCanvasManager) {
             window.G_vmlCanvasManager.initElement(canvas);
         }
-        
+
         var ctx = canvas.getContext('2d');
         ctx.strokeStyle = options.linkColor;
         ctx.lineWidth = options.linkWidth;
-        
+
         for (var i = 0, kid; kid = kids[i]; i++) {
             var kidX = kid.offsetLeft + kid.offsetWidth / 2 - node.kidsLeft;
-            
+
             ctx.beginPath();
             ctx.moveTo(x, 0);
             //ctx.lineTo(kidX, height);
@@ -166,7 +169,7 @@ var WebNodes = function(root, graph, options){
             ctx.closePath();
         }
     }
-    
+
     // Events
     var priority = 1;
     $('.pagination a').live('click', function(e) {
@@ -181,12 +184,14 @@ var WebNodes = function(root, graph, options){
         } else {
             node.priority = priority++;
         }
-        
-        $(document.body).css('min-height', $(document).height());        
-        $('canvas').remove();
-        $('.comment_container').hide();
-        $(root).show();
-        layout(root);
+
+        $(document.body).css('min-height', $(document).height());
+        container.children('canvas').remove();
+        container.children('.comment_container').hide();
+        layout();
         return false;
     });
 }
+
+})(jQuery);
+
