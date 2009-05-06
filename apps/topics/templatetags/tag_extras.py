51fe61cb78e4119c62be1ebbf1d35cbf48024be5
Chris Tan <csytan@gmail.com>
2009-05-06 09:50:10 -0700

    urlify-markdown space fix

--- apps/topics/templatetags/tag_extras.py
+++ apps/topics/templatetags/tag_extras.py
@@ -12,9 +12,9 @@ def timeago(value, arg=None):
 timeago.safe = False
 
 
-urlfinder = re.compile('(^http:\/\/\S+)')
+urlfinder = re.compile('^(http:\/\/\S+)')
 urlfinder2 = re.compile('\s(http:\/\/\S+)')
 @register.filter('urlify_markdown')
 def urlify_markdown(value):
     value = urlfinder.sub(r'<\1>', value)
-    return urlfinder2.sub(r'<\1>', value)
+    return urlfinder2.sub(r' <\1>', value)
