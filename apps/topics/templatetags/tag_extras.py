8e24e96df90ef2ab0c2209709eb2043cc4d2b309
Chris Tan <csytan@gmail.com>
2009-05-06 00:56:26 -0700

    Markdown tweaks.

--- apps/topics/templatetags/tag_extras.py
+++ apps/topics/templatetags/tag_extras.py
@@ -1,3 +1,5 @@
+import re
+
 from django import template
 from django.template import defaultfilters
 
@@ -8,3 +10,11 @@ def timeago(value, arg=None):
     timesince = defaultfilters.timesince(value, arg)
     return timesince.split(',')[0] + ' ago'
 timeago.safe = False
+
+
+urlfinder = re.compile('(^http:\/\/\S+)')
+urlfinder2 = re.compile('\s(http:\/\/\S+)')
+@register.filter('urlify_markdown')
+def urlify_markdown(value):
+    value = urlfinder.sub(r'<\1>', value)
+    return urlfinder2.sub(r'<\1>', value)
