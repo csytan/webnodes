d40ece32244125e8d746a61ca4d57fc8bb98a48f
Chris Tan <csytan@gmail.com>
2009-05-20 21:27:47 -0700

    Faster topic graph generation using "keys_only" query. Other refactoring stuff.

--- apps/topics/models.py
+++ apps/topics/models.py
@@ -1,6 +1,30 @@
+# Django imports
+from django.template.defaultfilters import slugify
+
 # Google imports
 from google.appengine.ext import db
 from google.appengine.api import memcache
+
+
+### Helper functions ###
+def score(points, submitted_date):
+    """
+    Reddit "hotness" algorithm from:
+    http://www.reddit.com/r/programming/comments/6ph35/reddits_collaborative_filtering_algorithm/c04is8d
+    """
+    def seconds_since_cutoff(date):
+        cutoff = datetime(2005, 12, 8, 7, 46, 43)
+        td = date - cutoff
+        seconds = td.days * 24 * 60 * 60
+        seconds += td.seconds
+        seconds += float(td.microseconds) / 1000000
+        return seconds
+
+    order = log(max(abs(points), 1), 10)
+    sign = 1 if points > 0 else -1 if points < 0 else 0
+    age = seconds_since_cutoff(submitted_date)
+    return round(order + sign * age / 45000, 7)
+
 
 
 ### Models ###
@@ -21,7 +45,7 @@ class Tag(db.Model):
         return cls.all().order('-count').fetch(50)
         
     @classmethod
-    def increment_tags(cls, tag_names):
+    def increment(cls, tag_names):
         """Increments the tag count by one"""
         tags = cls.get_by_key_name(tag_names)
 
@@ -33,45 +57,15 @@ class Tag(db.Model):
             save_tags.append(tag)
         db.put(save_tags)
 
-class VotableMixin(db.Model):
-    points = db.IntegerProperty(default=0)
-    score = db.FloatProperty(default=0.0)
-    
-    @staticmethod
-    def seconds_since_cutoff(date):
-        cutoff = datetime(2005, 12, 8, 7, 46, 43)
-        td = date - cutoff
-        seconds = td.days * 24 * 60 * 60
-        seconds += td.seconds
-        seconds += float(td.microseconds) / 1000000
-        return seconds
-    
-    @staticmethod
-    def score(points, submitted_date):
-        """
-        Reddit "hotness" algorithm from:
-        http://www.reddit.com/r/programming/comments/6ph35/reddits_collaborative_filtering_algorithm/c04is8d
-        """
-        order = log(max(abs(points), 1), 10)
-        sign = 1 if points > 0 else -1 if points < 0 else 0
-        age = seconds_since_cutoff(submitted_date)
-        return round(order + sign * age / 45000, 7)
-    
-    def vote_up(self):
-        user = Users.get_current_user()
-        email = user.email()
-        if email in self.votes_up:
-            self.votes_up.append(email)
-        self.put()
-    
 
-
-
-class Comment(VotableMixin):
-    author = db.StringProperty(default='anonymous')
+class Comment(db.Model):
+    author = db.StringProperty()
     body = db.TextProperty()
     topic_id = db.IntegerProperty()
     parent_id = db.IntegerProperty()
+    
+    points = db.IntegerProperty(default=0)
+    
     has_replies = db.BooleanProperty(default=False, indexed=False)
     reply_cache = db.ListProperty(int, indexed=False)
     
@@ -89,7 +83,7 @@ class Comment(VotableMixin):
         
         comment = Comment(
             author=author,
-            topic_id=self.id if isinstance(self, Topic) else self.topic_id,
+            topic_id=self.topic_id,
             parent_id=self.id,
             body=body
         )
@@ -102,12 +96,12 @@ class Comment(VotableMixin):
         if not self.has_replies:
             return []
 
-        query = Comment.all()
+        query = Comment.all(keys_only=True)
         query.filter('parent_id =', self.id)
         query.order('-updated')
-        replies = query.fetch(1000)
+        reply_keys = query.fetch(1000)
 
-        self.reply_cache = [reply.id for reply in replies]
+        self.reply_cache = [int(key.id()) for key in reply_keys]
         self.put()
         return self.reply_cache
 
@@ -115,15 +109,15 @@ class Comment(VotableMixin):
 class Topic(Comment):
     title = db.StringProperty()
     num_comments = db.IntegerProperty(default=0)
-    tags = db.StringListProperty()
+    tag = db.StringProperty()
     
     @classmethod
-    def create(cls, author, title, body, tags=None):
+    def create(cls, author, title, body, tag):
         topic = cls(
             author=author,
             title=title,
             body=body,
-            tags=[str(slugify(tag)) for tag in tags] if tags else []
+            tag=str(slugify(tag))
         )
         topic.put()
         return topic
@@ -134,25 +128,14 @@ class Topic(Comment):
         
     @classmethod
     def topics_by_tag(cls, tag):
-        query = cls.all().filter('tags =', tag)
+        query = cls.all().filter('tag =', tag)
         query.order('-created')
         return query.fetch(50)
-        
-    def add_tag(self, name):
-        if name in self.tags: return
-        
-        tag = Tag.get_or_insert(name)
-        tag.num_tagged += 1
-        tag.put()
-        
-        self.tags.append(name)
-        self.put()
     
-    def remove_tag(self, name):
-        self.tags = [tag for tag in self.tags if tag != name]
-        self.put()
-        
-        
+    @property
+    def topic_id(self):
+        return self.id
+    
     _comments = None
     @property
     def comments(self):
