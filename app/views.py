1c213cfb288b48f681474fe9416155930bdaaf36
Chris Tan <csytan@gmail.com>
2010-11-19 05:55:11 -0800

    better error handling

--- app/views.py
+++ app/views.py
@@ -180,6 +180,7 @@ class Submit(BaseHandler):
         and converts spaces to hyphens.
         http://code.djangoproject.com/svn/django/trunk/django/template/defaultfilters.py
         """
+        value = unicode(value)
         value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore')
         value = unicode(re.sub('[^\w\s-]', '', value).strip().lower())
         return re.sub('[-\s]+', '-', value)
