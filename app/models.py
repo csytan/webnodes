b5aa74d2cf2a0119dbbc9f5082b84eb8480b11c8
Chris Tan <csytan@gmail.com>
2010-11-25 20:46:02 -0800

    topic listings show points instead of author.  topic edits shown as such.  shorter inbox messages

--- app/models.py
+++ app/models.py
@@ -67,7 +67,7 @@ class Site(BaseModel):
         return db.run_in_transaction(txn)
         
     def hot_topics(self, page=0):
-        topics = self.topics.order('-updated').fetch(20, offset=page*20)
+        topics = self.topics.order('-score').fetch(20, offset=page*20)
         prefetch_refprop(topics, Topic.author)
         return topics
 
